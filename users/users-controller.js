bdfaffa78d2e0578a37b0a9e6f107d1438cfab39
shravangopalakrishnan <shravgop@gmail.com>
2023-06-11 00:50:31 -0400

    second commit

--- users/users-controller.js
+++ users/users-controller.js
@@ -1,54 +1,48 @@
-import people from './users.js'
-let users = people
-const UserController = (app) => {
-   app.get('/api/users', findUsers)
-   app.get('/api/users/:uid', findUserById);
-   app.post('/api/users', createUser);
-   app.delete('/api/users/:uid', deleteUser);
-   app.put('/api/users/:uid', updateUser);
+import people from './users.js';
+import * as usersDao from "./users-dao.js";
 
-}
-  
-const findUserById = (req, res) => {
-    const userId = req.params.uid;
-    const user = users
-      .find(u => u._id === userId);
-    res.json(user);
-}
-  
-const findUsers = (req, res) => {
-    const type = req.query.type
-    if(type) {
-      const usersOfType = users
-        .filter(u => u.type === type)
-      res.json(usersOfType)
-      return
-    }  
-    res.json(users)
-}
-const createUser = (req, res) => {
-    const newUser = req.body;
-    newUser._id = (new Date()).getTime() + '';
-    users.push(newUser);
-    res.json(newUser);
-}
+let users = people;
+
+const UserController = (app) => {
+  app.get('/api/users', findUsers);
+  app.get('/api/users/:uid', findUserById);
+  app.post('/api/users', createUser);
+  app.delete('/api/users/:uid', deleteUser);
+  app.put('/api/users/:uid', updateUser);
+};
 
 const updateUser = (req, res) => {
-    const userId = req.params['uid'];
-    const updates = req.body;
-    users = users.map((usr) =>
-      usr._id === userId ?
-        {...usr, ...updates} :
-        usr
-    );
-    res.sendStatus(200);
-}
-   
+  const userId = req.params.uid;
+  const updates = req.body;
+  const updatedUser = usersDao.updateUser(userId, updates);
+  req.session.currentUser = updatedUser;
+  console.log(users);
+  res.sendStatus(200);
+};
+
 const deleteUser = (req, res) => {
-    const userId = req.params['uid'];
-    users = users.filter(usr => usr._id !== userId);
-    res.sendStatus(200);
-}
-  
-  
-export default UserController
+  const userId = req.params.uid;
+  users = users.filter((user) => user._id !== userId);
+  res.sendStatus(200);
+};
+
+const createUser = (req, res) => {
+  const newUser = req.body;
+  newUser._id = new Date().getTime().toString();
+  users.push(newUser);
+  console.log(users);
+  res.json(newUser);
+};
+
+const findUserById = (req, res) => {
+  const userId = req.params.uid;
+  const user = users.find((user) => user._id === userId);
+  res.json(user);
+};
+
+const findUsers = (req, res) => {
+  const allUsers = usersDao.findAllUsers();
+  res.json(allUsers);
+};
+
+export default UserController;
--- users/users-dao.js
+++ users/users-dao.js
@@ -1,43 +1,47 @@
-let users = [];
+import people from './users.js';
 
+let users = people;
 
 export const findAllUsers = () => users;
 
-
 export const findUserById = (uid) => {
- const index = users.findIndex((u) => u._id === uid);
- if (index !== -1) return users[index];
- return null;
+  const index = users.findIndex((u) => u._id === uid);
+  if (index !== -1) return users[index];
+  return null;
 };
-
 
 export const findUserByUsername = (username) => {
- const index = users.findIndex((u) => u.username === username);
- if (index !== -1) return users[index];
- return null;
+  const index = users.findIndex((u) => u.username === username);
+  if (index !== -1) return users[index];
+  return null;
 };
-
 
 export const findUserByCredentials = (username, password) => {
- const index = users.findIndex.find((u) => u.username === username && u.password === password);
- if (index !== -1) return users[index];
- return null;
+  const index = users.findIndex((u) => u.username === username && u.password === password);
+  if (index !== -1) return users[index];
+  return null;
 };
 
-
-export const createUser = (user) => users.push(user);
-
+export const createUser = (user) => {
+  const newUser = { ...user, _id: new Date().getTime().toString() };
+  users.push(newUser);
+  return newUser;
+};
 
 export const updateUser = (uid, user) => {
- const index = users.findIndex((u) => u._id === uid);
- users[index] = { ...users[index], ...user };
- return {status: 'ok'}
+  const index = users.findIndex((u) => u._id === uid);
+  if (index !== -1) {
+    users[index] = { ...users[index], ...user };
+    return users[index];
+  }
+  return null;
 };
-
 
 export const deleteUser = (uid) => {
- const index = users.findIndex((u) => u._id === uid);
- users.splice(index, 1);
- return {status: 'ok'}
+  const index = users.findIndex((u) => u._id === uid);
+  if (index !== -1) {
+    users.splice(index, 1);
+    return { status: 'ok' };
+  }
+  return null;
 };
-
